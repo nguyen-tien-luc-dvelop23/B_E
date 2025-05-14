b944138e15c60b13f1b80306e43697fac23bc1fb
nguyen-tien-luc-dvelop23 <1771020440@dnu.edu.vn>
2025-05-14 21:37:26 +0700

    lab2

---
+++ LAB2/Controllers/TodoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LAB2.Controllers
+{
+    public class TodoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Todo/Add
+        public IActionResult Add()
+        {
+            return View();
+        }
+        // GET: Todo/Edit/1
+        public IActionResult Edit(int id)
+        {
+            ViewBag.TaskId = id; // Truyền ID của công việc cần sửa
+            return View();
+        }
+
+
+    }
+}
